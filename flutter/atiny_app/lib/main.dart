125052b792f1965f55400b79bc6a1f7cfdff2974
Marianne002 <gdj.marianne@gmail.com>
2023-07-07 16:16:14 +0200

    Final push

--- flutter/atiny_app/lib/main.dart
+++ flutter/atiny_app/lib/main.dart
@@ -20,6 +20,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Atiny",
       theme: ThemeData(
         fontFamily: 'Handlee',
